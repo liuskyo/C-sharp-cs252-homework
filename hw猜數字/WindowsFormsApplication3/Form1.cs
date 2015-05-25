37e633fe50443585762ea366dbf7666457fe3279
liuskyo <b98801039@ntu.edu.tw>
2015-05-26 00:45:55 +0800

    homework
    homework

---
+++ hw猜數字/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        string anser = makeAnwserNumber();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            guessNumberGame game = new guessNumberGame();
+            string str = game.guess(anser, textBox1.Text);
+            MessageBox.Show(str);
+        }
+
+       public static string makeAnwserNumber()
+        {
+            Random random=new Random();
+            List<int> unuseNumber =new List<int>();
+            List<int> useNumber = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                unuseNumber.Add(i);
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                int temp;
+                temp = random.Next(0, unuseNumber.Count);
+                useNumber.Add(unuseNumber[temp]);
+                unuseNumber.RemoveAt(temp);
+
+            }
+            string answerString;
+            answerString = String.Format("{0}{1}{2}{3}", useNumber[0], useNumber[1], useNumber[2], useNumber[3]);
+
+
+
+            return answerString;
+            
+
+        }
+
+       private void button2_Click(object sender, EventArgs e)
+       {
+           guessNumberGame computerGame = new guessNumberGame();
+           computerGame.computerGuess(anser);
+       }
+
+
+    }
+}
---
+++ hw猜數字/WindowsFormsApplication3/guessNumberGame.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    class guessNumberGame
+    {
+       public string guess(string anwserNumber,string guessNumber){
+           int A, B;
+           A=0;
+           B=0;
+
+           for (int i = 0; i < anwserNumber.Count(); i++) {
+               for (int j = 0; j < guessNumber.Count(); j++) {
+                   if (anwserNumber[i] == guessNumber[j]) {
+                       B++;
+                   }
+               }
+           }
+               for (int i = 0; i < anwserNumber.Count(); i++)
+               {
+                   if (guessNumber[i] == anwserNumber[i])
+                   {
+                       B--;
+                       A++;
+                   }
+               }
+               if (A == 4)
+               {
+                   return "猜對了!!";
+               }
+               else {
+                   return String.Format("{0}A{1}B", A, B);
+               
+               }
+
+       }
+
+       public void computerGuess(string anwserNumber)
+       {
+          List<string> unuseString=new List<string>();
+          int i, j, k, l;
+          for (i = 0; i <= 9; i++) {
+              for (j = 0; j <= 9; j++)
+              {
+                  for (k = 0; k <= 9; k++)
+                  {
+                      for (l = 0; l <= 9; l++)
+                      {
+                          if ((i != j) && (i != k))
+                          {
+                              if ((i != l) && (j != k))
+                              {
+                                  if ((j != l) && (k != l))
+                                  {
+                                      string str = String.Format("{0}{1}{2}{3}", i, j, k, l);
+                                      unuseString.Add(str);
+                                  }
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+
+          string computerGuessString;
+          string computerGuessResponse="";
+          while (computerGuessResponse != "猜對了!!")
+          {
+              Random random = new Random();
+              int randomint = random.Next(0, unuseString.Count);
+              computerGuessString = unuseString[randomint];
+              computerGuessResponse = guess(anwserNumber, computerGuessString);
+              MessageBox.Show(String.Format("電腦猜:{0},結果:{1}",computerGuessString,computerGuessResponse));
+            //  Console.WriteLine("電腦猜:{0}", computerGuessString);
+            //  Console.WriteLine("結果:{0}", computerGuessResponse);
+              if (computerGuessResponse != "猜對了!!")
+              {
+                  int AplusB = int.Parse(String.Format("{0}",computerGuessResponse[0]));
+                  AplusB = AplusB + int.Parse(String.Format("{0}", computerGuessResponse[2]));
+                  unuseString.Remove(computerGuessString);
+                  for (int t = 0; t < unuseString.Count; t++)
+                  {
+                      string temStr = guess(computerGuessString, unuseString[t]);
+                      int temAplusB = int.Parse(String.Format("{0}", temStr[0]));
+                      temAplusB = temAplusB + int.Parse(String.Format("{0}", temStr[2]));
+                      if (temAplusB > AplusB)
+                      {
+                          unuseString.RemoveAt(t);
+                      }
+                  }
+
+
+              }
+          }
+
+
+
+
+
+
+
+
+
+
+       }
+
+
+
+
+
+
+
+    }
+}
