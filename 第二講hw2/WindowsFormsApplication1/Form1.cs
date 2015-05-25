adb1ba8944ccd2bdb53a69173dcb420051096b91
liuskyo <b98801039@ntu.edu.tw>
2015-05-25 22:41:22 +0800

    cs252homework
    homework

---
+++ 第二講hw2/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int sex = random.Next(1, 2);
+            string show;
+            if (sex == 1)
+            {
+                show=String.Format("{0}是男生，恭喜你猜對了!",name.Text);
+                MessageBox.Show(show);
+            }
+            else
+            {
+                show = String.Format("{0}是女生，你猜錯了!", name.Text);
+                MessageBox.Show(show);
+            }
+
+        }
+
+        private void female_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int sex = random.Next(1, 2);
+            string show;
+            if (sex == 2)
+            {
+                show = String.Format("{0}是女生，恭喜你猜對了!", name.Text);
+                MessageBox.Show(show);
+            }
+            else
+            {
+                show = String.Format("{0}是男生，你猜錯了!", name.Text);
+                MessageBox.Show(show);
+            }
+        }
+    }
+}
---
+++ 第四講隨堂練習03/WindowsFormsApplication1/Empolyee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Empolyee
+    {
+        private int baseSalary;
+        private int benefit;
+
+        public int BaseSalary
+        {
+            get { return baseSalary; }
+            set {
+               baseSalary= (value > 0) ? value : 0;
+            }
+        }
+
+
+        public int Benefit
+        {
+            get { return benefit; }
+            set { benefit = value; }
+        }
+  
+
+        public int Salary
+        {
+            get { return baseSalary + benefit; }
+          
+        }
+    }
+}
